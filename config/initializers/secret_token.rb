14acafae0cd819ac519a4d481b56fb32db258621
tomayday <maydayjust@gmail.com>
2012-12-28 00:32:30 +0800

    second:add rails base files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ituqu::Application.config.secret_token = '2195c94b86e59cda7e152d69792782d2c24fa0def6706af046adb9c29516ada0cb3ad7d2069fc786055eaf98559fc68ddcbc30d9064828dc5a0c68bd7062eede'
